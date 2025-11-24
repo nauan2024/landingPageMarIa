77c7f4f31be114c365e10cb818352bf241bb0035
n1c0ly <santosnico048@gmail.com>
2025-11-24 13:21:53 +0000

    Tentando fazer cards

--- MarIa/src/assets/components/layouts/cards/index.jsx
+++ MarIa/src/assets/components/layouts/cards/index.jsx
@@ -1,10 +1,28 @@
 import './style.css'
-import bgCards from '../../../images/bgCards.svg'
+import innovationIcon from '../../../images/innovationIcon.svg'
+import simplicityIcon from '../../../images/simplicityIcon.svg'
 
 export const Cards = ()=>{
     return(
-<cards>
-<h2>Teste</h2>
-</cards>
+<section>
+<div className="doubleCardContainer">
+    <div className="doubleCard">
+        <img src={simplicityIcon} alt="" />
+        <h3>Simplicidade</h3>
+    </div>
+    <h4>Diversão Garantida</h4>
+    <p>Gráficos minimalistas desenvolvidos com pixel art totalmente artesanal, criando um visual único que combina simplicidade, estilo retrô e personalidade em cada detalhe.</p>
+</div>
+
+<div className="doubleCardContainer">
+    <div className="doubleCard">
+        <img src={innovationIcon} alt="" />
+        <h3>Inovação</h3>
+    </div>
+    <h4>Concientização com as ODS</h4>
+    <p>Uma integração verdadeira entre conscientização e sustentabilidade, unindo informação, propósito e impacto para criar uma experiência que inspira mudanças reais.</p>
+</div>
+
+</section>
     );
 };
