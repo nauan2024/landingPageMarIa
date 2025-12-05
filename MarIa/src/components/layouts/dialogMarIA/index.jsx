1620be09930933c1ad404b5c0c5389d4e53894c4
BiellNonato <gabrielnonato53@gmail.com>
2025-12-05 16:23:50 -0300

    feat: responsive footer/dialog/questions

--- MarIa/src/components/layouts/dialogMarIA/index.jsx
+++ MarIa/src/components/layouts/dialogMarIA/index.jsx
@@ -1,9 +1,13 @@
-import imgdialog from "../../../assets/images/grupo-nauan.svg"
+import imgmap from "../../../assets/images/map2.svg"
+import ballon from "../../../assets/images/falaMarIa.svg"
+import MariaHabla from "../../../assets/images/MarIAhabla.svg"
 import "./style.css"
 export const DialogMarIA = () => {
     return(
         <section className="sectionDialog">
-        <img src={imgdialog} alt="" />
+            <img className="imgmap" src={imgmap} alt="" />
+            <img className="MariaHabla" src={MariaHabla} alt="" />
+            <img className="ballon" src={ballon} alt="" />
         </section>
     )
 }
